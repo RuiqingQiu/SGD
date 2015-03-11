4d6cd2e1d15cdd3b5c20caf0c77728687b2f63ce
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-03-10 21:42:52 -0700

    distance measurement need to be fixed

--- sgd.py
+++ sgd.py
@@ -1,4 +1,5 @@
 import math
+import os.path
 import numpy as np
 import re
 
@@ -14,92 +15,135 @@ def frobenius_norm(M1, M2):
 d = 15
 # k is the top pricipal calculated by the eigenvectors, right now just fix it
 k = 5
-# fix the learning rate 
+# fix the learning rate
 learning_rate = 0.001
 # preprocess the data
 data_set = []
-with open('adult.data.txt') as data:
-    for line in data:
-        # '\s' matches whitespace
-        tmp = re.sub(r'\s', '', line).split(',')
-        data_set.append(tmp)
-#print data_set
+data_set_done = []
+if os.path.exists('new_data'):
+    with open('new_data') as data:
+        for line in data:
+            data_set_done.append(map(float, line.split()))
 
-feature_vector_size = len(data_set[0])
-print "feature vector is  " ,feature_vector_size
-print "data set size is ", len(data_set)
-tmp = data_set[0]
+else:
+    with open('adult.data.txt') as data:
+        for line in data:
+            # '\s' matches whitespace
+            tmp = re.sub(r'\s', '', line).split(',')
+            data_set.append(tmp)
+    print data_set
 
-# find out which feature is not a digit
-non_digit_index = []
-for i in range (0,feature_vector_size):
-    if tmp[i].isdigit():
-        continue
-    else:
-        non_digit_index.append(i)
-print non_digit_index
+    feature_vector_size = len(data_set[0])
+    print "feature vector is  " ,feature_vector_size
+    print "data set size is ", len(data_set)
+    tmp = data_set[0]
 
-# make each string feature to become a number
-
-# Copy
-modified_data_set= []
-for item in data_set:
-    modified_data_set.append(item)
-    
-#print modified_data_set
-# for all the non_digit index, count how many differnt items
-for i in non_digit_index:
-    # clear the item list to find different string for same feature
-    items = []
-    # loop through the whole data set
-    for vector in data_set:
-        # get the corresponsding item
-        tmp = vector[i]
-        # it already in the list
-        if tmp in items:
+    # find out which feature is not a digit
+    non_digit_index = []
+    for i in range (0,feature_vector_size):
+        if tmp[i].isdigit():
             continue
         else:
-            # otherwise append to the list
-            items.append(tmp)
-    print items
-    # loop through the whole data set to replace the feature
+            non_digit_index.append(i)
+    print non_digit_index
+
+    # make each string feature to become a number
+
+    # Copy
+    modified_data_set= []
+    for item in data_set:
+        modified_data_set.append(item)
+
+
+    print "data before processed dimension is ", len(modified_data_set[0])
+    #print modified_data_set
+    item_list = []
+    count = 0
+    dimension_to_increase = 0
+    # for all the non_digit index, count how many differnt items
+    for i in non_digit_index:
+        # clear the item list to find different string for same feature
+        items = []
+        # loop through the whole data set
+        for vector in data_set:
+            # get the corresponsding item
+            tmp = vector[i]
+            # it already in the list
+            if tmp in items:
+                continue
+            else:
+                # otherwise append to the list
+                items.append(tmp)
+        dimension_to_increase = dimension_to_increase + len(items)-1
+        #print "item is "
+        #print items
+        #print "\n\n"
+        # loop through the whole data set to replace the feature
+        for j in range(len(modified_data_set)):
+            v = modified_data_set[j]
+            #print v
+            # get the item in the vector
+            #print "accessing ", i+count
+            tmp = v[i+count]
+            index  = items.index(tmp)
+            front = v[0:i+count]
+            end = v[i+1+count:len(v)]
+            zeros = [0]*len(items)
+            zeros[index] = 1
+            front.extend(zeros)
+            front.extend(end)
+            modified_data_set[j] = front
+            #print "result is ", modified_data_set[j]
+            #v[i] = float(index)
+        count = count + len(items)-1
     for v in modified_data_set:
-        # get the item in the vector
-        tmp = v[i]
-        index  = items.index(tmp)
-        v[i] = float(index)
-data_set_done = []
-for v in modified_data_set:
-    v = map(float, v)
-    data_set_done.append(v)
-#print data_set_done
-print "size of data set done is ", len(data_set_done)
+        v = map(float, v)
+        data_set_done.append(v)
+    print "dimension to increase ", dimension_to_increase
+    print "data processed dimension is ", len(data_set_done[0])
+    #print data_set_done
+    print "size of data set done is ", len(data_set_done)
+
+f = open('new_data', 'w')
+for vector in data_set_done:
+    for num in vector:
+        f.write(str(num)+" ")
+    f.write("\n")
+f.close()
 
 # U and V are d x k dimension matrix
-v = np.random.rand(d,k)
+v = np.random.rand(len(data_set_done[0]),k)
 print "V start with, ", v
-u = np.random.rand(d,k)
+u = np.random.rand(len(data_set_done[0]),k)
 print "U start with, ", u
 
-print "result of the caulcation is ", np.transpose(data_set_done[0]) 
+print "result of the caulcation is ", np.transpose(data_set_done[0])
 
+M = np.array(data_set_done[0]) * np.transpose(np.array(data_set_done[0]))
+for i in range(1, len(data_set_done)):
+    M = M + np.array([data_set_done[i]]) * np.transpose(np.array([data_set_done[i]]))
+M = M / len(data_set_done)
+print "M is "
+print M
 # SGD function
 # run the whole optimization process 10 times
 for j in range(0, 10):
-    # do u 100 rounds 
-    for counter in range (0,99):
+    # do u 100 rounds
+    for counter in range (0,10):
+        print counter
         # update u len(data_set) iterations
         for t in range(0, len(data_set_done)):
             #print (data_set[t] * np.transpose(data_set[t]) - np.dot(u,np.transpose(v)))
             # need to do np.dot() for matrix multiplication
             u = u - learning_rate * np.dot((data_set_done[t] * np.transpose(data_set_done[t]) - np.dot(u,np.transpose(v))), v)
     # do v 100 rounds
-    for counter in range (0,99):
+    for counter in range (0,10):
+        print count
         # update v len(data_set) iterations
         for t in range(0, len(data_set_done)):
             v = v - learning_rate * np.dot((data_set_done[t] * np.transpose(data_set_done[t]) - np.dot(u, np.transpose(v))), u)
-    #print frobenius_norm(M, np.dot(u,np.transpose(v)))
+    print frobenius_norm(M, np.dot(u,np.transpose(v)))
 
 
-    
+
 #print u * np.transpose(v)
